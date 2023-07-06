68bdbcc975ce1bdad1db0abe4bf445bbcaabe0e6
Piotr Piwoński <piwonskp@gmail.com>
2023-07-06 12:58:14 +0200

    Add missing import

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -3,6 +3,7 @@
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import "hardhat/types/config";
 import "hardhat/types/runtime";
+import { Table } from "./types";
 
 declare module "hardhat/types/config" {
   export interface ProjectPathsUserConfig {
