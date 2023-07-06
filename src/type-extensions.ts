a51bbc1ab117d2f84eda7d03f6cc1f8fe97c7e79
Piotr Piwoński <piwonskp@gmail.com>
2023-07-06 11:50:54 +0200

    Update type-extensions.ts with getLayout function

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -19,6 +19,7 @@ declare module "hardhat/types/runtime" {
   export interface HardhatRuntimeEnvironment {
     storageLayout: {
       export: () => Promise<void>;
+      getLayout: () => Promise<Table>;
     };
   }
 }
